d649f9cd6ef0151b3b6390a0d2fdc3d9a3c7be9f
Micah <micah@asuh.com>
2012-06-30 11:33:32 +0100

    Update to latest H5Bp code

--- _CHILD_THEME/functions.php
+++ _CHILD_THEME/functions.php
@@ -3,7 +3,7 @@
 // custom admin login logo for child theme
 function annex_child_custom_login_logo() {
 	echo '<style type="text/css">
-	h1 a { background-image: url('.get_bloginfo('stylesheet_directory').'/img/custom-login-logo.png) !important; }
+	h1 a { background-image: url('.get_bloginfo('stylesheet_directory').'/images/custom-login-logo.png) !important; }
 	</style>';
 }
 add_action('login_head', 'annex_child_custom_login_logo');
